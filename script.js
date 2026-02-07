463a0a3893540b98516cb0204e784723a4454745
Randy2222222 <randyjpollitt@yahoo.com>
2026-02-07 18:34:05 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+JavaScript 
+document.addEventListener('DOMContentLoaded', () => {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    let cartLink = document.getElementById('cartLink');
+    if (cartLink) {
+        cartLink.textContent = `My Cart (${cart.length})`;
+    }
+});
